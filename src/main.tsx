aaf893f597701e21b92ba259b3cb24dbc2233f7a
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-04-03 17:32:38 -0400

    feat: added apollo provider and client

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,18 @@ import { NextUIProvider } from '@nextui-org/react';
 import './index.css';
 import { router } from './router';
 import { RouterProvider } from 'react-router-dom';
-import { TanStackProvider } from './plugins/TanStackProvider';
+import { TanStackProvider } from './plugins/tanstackquery/TanStackProvider';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './plugins/apollo/apollo';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <TanStackProvider>
-      <NextUIProvider>
-        <RouterProvider router={router} />
-      </NextUIProvider>
-    </TanStackProvider>
+    <ApolloProvider client={client}>
+      <TanStackProvider>
+        <NextUIProvider>
+          <RouterProvider router={router} />
+        </NextUIProvider>
+      </TanStackProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
