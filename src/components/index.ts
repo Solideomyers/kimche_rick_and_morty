ecbe95ba58a8a836306f4bb578460293d267cd9a
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-04-03 18:00:05 -0400

    feat: index by components centralize

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+export { CharacterShow } from './characters/CharacterShow';
+export { CardCharacter } from './characters/CardCharacter';
+export { AllCharacters } from './characters/AllCharacters';
+
+export { Filter } from './filter/Filter';
+
+export { SearchBar } from './navbar/SearchBar';
+
+export { ListboxWrapper } from './ui/ListboxWrapper';
+export { ModalButton } from './ui/ModalButton';
+export { ModalCard } from './ui/ModalCard';
+export { ModalContainer } from './ui/ModalContainer';
+export { SelectProperty } from './ui/SelectProporty';
